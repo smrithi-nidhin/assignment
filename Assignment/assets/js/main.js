39e1e62e677c2886068d873552b00b76449d5cab
Smrithi <smrithiraveendrann@gmail.com>
2021-01-27 09:21:43 +0300

    assignment

---
+++ Assignment/assets/js/main.js
@@ -0,0 +1,15 @@
+
+!(function($) {
+  "use strict";
+
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
+
+})(jQuery);
